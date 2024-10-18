7dca1ea4daabfcdbd41785e9c6c98841e63a99b7
Vikash Mahato <vikashmahatoravo@gmail.com>
2024-10-18 09:51:29 +0530

    Add new file

---
+++ object.py
@@ -0,0 +1,14 @@
+a=70564432314567788
+b=506778905634235678
+sum=a+b
+div=a/b
+pro=a*b
+diff=a-b
+old = False
+print("the sum is", sum)
+print("the division is", div)
+print("the product is", pro)
+print("the diffrence is", diff)
+print("the type is", type(a))
+print("The type of old is", type(old))
+
